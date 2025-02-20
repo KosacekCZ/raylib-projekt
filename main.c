c8d7f6cdca2c9edb4df98b59bbd9a98e30d9acfe
mikolasp <kosacekcz@gmail.com>
2025-02-20 09:45:34 +0100

    Working pong

--- main.c
+++ main.c
@@ -1,56 +1,102 @@
-#include "raylib.h"
-#include "Sorting.h"
-#include <stdlib.h>
+ #include "raylib.h"
+ #include <stdlib.h>
 
 
-void draw_data(int data[]) {
-  for (int i = 0; i < 10; i++) {
-      DrawRectangle((30 + 5) * i, GetScreenHeight() - (data[i] * 30), 30, data[i] * 30, RED);
-      }
-  }
-
-void sort_array(int arr[], int size) {
-
-}
-
-
-int main() {
-    int arr[] = {8, 9, 1, 4, 0, 7, 6, 4, 9, 1, 5, 3, 2, 8, 9, 1};
-
-    InitWindow(800,600,"Raylib");
-    SetTargetFPS(60);
-    Texture2D texture = LoadTexture("assets/ctverec.png"); //načtení textury na GPU
-
-    // Main game loop
-    // DrawRectangle(100, 100, 200, 200, GRAY);
-
-    int i = 0;
-    while (!WindowShouldClose())
-    {
-        // Start drawing
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        for (int j = 0; j < 16 - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap the numbers
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-            draw_data(arr);
-        }
-        i++;
-        WaitTime(0.5);
+ #define border_sx 0
+ #define border_sy 0
+ #define border_ex 800
+ #define border_ey 600
+ #define moveStep 5
 
 
 
-        // vykreslení textury
-        EndDrawing();
-    }
+ int main() {
 
-    UnloadTexture(texture); // vymazání textury z paměti
-    CloseWindow();
+     struct Square {
+         int x;
+         int y;
+         int w;
+         int h;
+         Vector2 vec;
+         Color color;
+     };
+
+     struct Pad {
+       int x;
+       int y;
+       int w;
+       int h;
+       Color color;
+     };
+
+     struct Square * s = (struct Square *)malloc(sizeof(struct Square));
+     s->x = 200;
+     s->y = 300;
+     s->w = 50;
+     s->h = 50;
+     s->color = WHITE;
+     s->vec = (Vector2){4.0f, -4.0f};
+
+     struct Pad * p1 = (struct Pad *)malloc(sizeof(struct Pad));
+     struct Pad * p2 = (struct Pad *)malloc(sizeof(struct Pad));
+     p1->w = 20;
+     p2->w = 20;
+     p1->h = 100;
+     p2->h = 100;
+     p1->x = 50;
+     p2->x = 800 - p2->w - 50;
+     p1->y = GetScreenHeight() + p1->h / 2;
+     p2->y = GetScreenHeight() + p2->h / 2;
+     p1->color = WHITE;
+     p2->color = WHITE;
+
+     InitWindow(800,600,"Raylib");
+     SetTargetFPS(60);
+
+     while (!WindowShouldClose())
+     {
+         // Start drawing
+         BeginDrawing();
+         ClearBackground(BLACK);
+
+         DrawRectangle(s->x, s->y, s->w, s->h, s->color);
+         DrawRectangle(p1->x, p1->y, p1->w, p1->h, s->color);
+         DrawRectangle(p2->x, p2->y, p2->w, p2->h, s->color);
+
+         s->x += s->vec.x;
+         s->y += s->vec.y;
+
+         if (p1->y > 0 && IsKeyDown(KEY_W)) p1-> y-= moveStep;
+         if (p1->y + p1->h < border_ey && IsKeyDown(KEY_S)) p1-> y+= moveStep;
+         if (p2->y > 0 && IsKeyDown(KEY_UP)) p2-> y-= moveStep;
+         if (p2->y + p2->h < border_ey && IsKeyDown(KEY_DOWN)) p2-> y+= moveStep;
+
+         if (s->y <= border_sy) s->vec.y *= -1;
+         //if ((s->x + s->w) >= border_ex) s->vec.x *= -1;
+         if ((s->y + s->h) >= border_ey) s->vec.y *= -1;
+         //if (s->x <= border_sx) s->vec.x *= -1;
 
 
-    return 0;
-}
+         if (s->x <= border_sx) CloseWindow();
+         if (s->x + s->w >= border_ex) CloseWindow();
+
+         if ((s->y + s->h >= p1->y && s->y <= p1->y + p1->h) && s->x <= p1->x + p1->w) {
+           s->vec.x *= -1;
+           }
+
+         if ((s->y + s->h >= p2->y && s->y <= p2->y + p2->h) && s->x + s->w >= p2->x) {
+             s->vec.x *= -1;
+         }
+
+
+
+
+
+         // vykreslení textury
+         EndDrawing();
+     }
+
+     CloseWindow();
+
+     return 0;
+ }
