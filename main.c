6b6383c6967159e42282c2a0be0ffa65f3b3a829
mikolasp <kosacekcz@gmail.com>
2025-02-27 09:45:12 +0100

    Working snek

--- main.c
+++ main.c
@@ -1,102 +1,141 @@
- #include "raylib.h"
- #include <stdlib.h>
+#include "raylib.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <time.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define SNAKE_SIZE 20
+
+struct SnakeTile {
+    Vector2 vec;
+    Color color;
+};
+
+struct Apple {
+    Vector2 position;
+    Color color;
+};
+
+struct SnakeTile *snake = NULL;
+int snakeLength = 1;
+struct Apple apple;
+char close = 1;
+int direction = 0;
+
+void reallocSnakeArr(struct SnakeTile **arr, int size) {
+    if (arr == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    struct SnakeTile *temp = (struct SnakeTile *)realloc(*arr, sizeof(struct SnakeTile) * size);
+    if (temp == NULL) {
+        exit(EXIT_FAILURE);
+    }
+    *arr = temp;
+}
+
+void spawnSnake(int x, int y) {
+    snake[0].vec = (Vector2){x, y};
+    snake[0].color = GREEN;
+}
+
+void spawnApple() {
+    apple.position = (Vector2){
+        (float)(rand() % (SCREEN_WIDTH / SNAKE_SIZE)) * SNAKE_SIZE,
+        (float)(rand() % (SCREEN_HEIGHT / SNAKE_SIZE)) * SNAKE_SIZE
+    };
+    apple.color = RED;
+}
+
+void moveSnake() {
+    for (int i = snakeLength - 1; i > 0; i--) {
+        snake[i].vec = snake[i - 1].vec;
+        snake[i].color = snake[i - 1].color;
+    }
 
 
- #define border_sx 0
- #define border_sy 0
- #define border_ex 800
- #define border_ey 600
- #define moveStep 5
+    if (direction == 0) {
+        snake[0].vec.x += SNAKE_SIZE;
+    } else if (direction == 1) {
+        snake[0].vec.y -= SNAKE_SIZE;
+    } else if (direction == 2) {
+        snake[0].vec.x -= SNAKE_SIZE;
+    } else if (direction == 3) {
+        snake[0].vec.y += SNAKE_SIZE;
+    }
+}
+
+void checkAppleCollision() {
+    if (CheckCollisionRecs((Rectangle){snake[0].vec.x, snake[0].vec.y, SNAKE_SIZE, SNAKE_SIZE},
+                           (Rectangle){apple.position.x, apple.position.y, SNAKE_SIZE, SNAKE_SIZE})) {
+
+        snakeLength++;
+        reallocSnakeArr(&snake, snakeLength);
+        spawnApple();
+    }
+}
+
+int checkWallCollision() {
+    if (snake[0].vec.x < 0 || snake[0].vec.x >= SCREEN_WIDTH ||
+        snake[0].vec.y < 0 || snake[0].vec.y >= SCREEN_HEIGHT) {
+        return 1;
+    }
+    return 0;
+}
+
+int checkSelfCollision() {
+    for (int i = 1; i < snakeLength; i++) {
+        if (snake[i].vec.x == snake[0].vec.x && snake[i].vec.y == snake[0].vec.y) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snek");
+    SetTargetFPS(10);
+    srand((unsigned int)time(NULL));
 
 
+    snake = (struct SnakeTile *)malloc(sizeof(struct SnakeTile) * snakeLength);
+    if (snake == NULL) {
+        exit(EXIT_FAILURE);
+    }
 
- int main() {
+    spawnSnake(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+    spawnApple();
 
-     struct Square {
-         int x;
-         int y;
-         int w;
-         int h;
-         Vector2 vec;
-         Color color;
-     };
+    // Main game loop
+    while (!WindowShouldClose() && close) {
 
-     struct Pad {
-       int x;
-       int y;
-       int w;
-       int h;
-       Color color;
-     };
-
-     struct Square * s = (struct Square *)malloc(sizeof(struct Square));
-     s->x = 200;
-     s->y = 300;
-     s->w = 50;
-     s->h = 50;
-     s->color = WHITE;
-     s->vec = (Vector2){4.0f, -4.0f};
-
-     struct Pad * p1 = (struct Pad *)malloc(sizeof(struct Pad));
-     struct Pad * p2 = (struct Pad *)malloc(sizeof(struct Pad));
-     p1->w = 20;
-     p2->w = 20;
-     p1->h = 100;
-     p2->h = 100;
-     p1->x = 50;
-     p2->x = 800 - p2->w - 50;
-     p1->y = GetScreenHeight() + p1->h / 2;
-     p2->y = GetScreenHeight() + p2->h / 2;
-     p1->color = WHITE;
-     p2->color = WHITE;
-
-     InitWindow(800,600,"Raylib");
-     SetTargetFPS(60);
-
-     while (!WindowShouldClose())
-     {
-         // Start drawing
-         BeginDrawing();
-         ClearBackground(BLACK);
-
-         DrawRectangle(s->x, s->y, s->w, s->h, s->color);
-         DrawRectangle(p1->x, p1->y, p1->w, p1->h, s->color);
-         DrawRectangle(p2->x, p2->y, p2->w, p2->h, s->color);
-
-         s->x += s->vec.x;
-         s->y += s->vec.y;
-
-         if (p1->y > 0 && IsKeyDown(KEY_W)) p1-> y-= moveStep;
-         if (p1->y + p1->h < border_ey && IsKeyDown(KEY_S)) p1-> y+= moveStep;
-         if (p2->y > 0 && IsKeyDown(KEY_UP)) p2-> y-= moveStep;
-         if (p2->y + p2->h < border_ey && IsKeyDown(KEY_DOWN)) p2-> y+= moveStep;
-
-         if (s->y <= border_sy) s->vec.y *= -1;
-         //if ((s->x + s->w) >= border_ex) s->vec.x *= -1;
-         if ((s->y + s->h) >= border_ey) s->vec.y *= -1;
-         //if (s->x <= border_sx) s->vec.x *= -1;
+        if ((IsKeyPressed(KEY_D) || IsKeyDown(KEY_D)) && direction != 2) direction = 0;
+        if ((IsKeyPressed(KEY_W) || IsKeyDown(KEY_W)) && direction != 3) direction = 1;
+        if ((IsKeyPressed(KEY_A) || IsKeyDown(KEY_A)) && direction != 0) direction = 2;
+        if ((IsKeyPressed(KEY_S) || IsKeyDown(KEY_S)) && direction != 1) direction = 3;
 
 
-         if (s->x <= border_sx) CloseWindow();
-         if (s->x + s->w >= border_ex) CloseWindow();
+        moveSnake();
+        checkAppleCollision();
 
-         if ((s->y + s->h >= p1->y && s->y <= p1->y + p1->h) && s->x <= p1->x + p1->w) {
-           s->vec.x *= -1;
-           }
+        if (checkWallCollision() || checkSelfCollision()) {
+            close = 0;
+        }
 
-         if ((s->y + s->h >= p2->y && s->y <= p2->y + p2->h) && s->x + s->w >= p2->x) {
-             s->vec.x *= -1;
-         }
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        for (int i = 0; i < snakeLength; i++) {
+            DrawRectangleV(snake[i].vec, (Vector2){SNAKE_SIZE, SNAKE_SIZE}, snake[i].color);
+        }
+
+        DrawRectangleV(apple.position, (Vector2){SNAKE_SIZE, SNAKE_SIZE}, apple.color);
+        EndDrawing();
+    }
 
 
-
-
-
-         // vykreslení textury
-         EndDrawing();
-     }
-
-     CloseWindow();
-
-     return 0;
- }
+    free(snake);
+    CloseWindow();
+    return 0;
+}
